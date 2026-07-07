0869caf36c6de95c81eac060380ee98aca741c26
IvamG-A <golyak.1@mail.ru>
2026-07-07 20:45:29 +0300

    Исправление: замена presence_of_element_located на element_to_be_clickable в checkout_page

--- 07_lesson/pages/checkout_page.py
+++ 07_lesson/pages/checkout_page.py
@@ -8,10 +8,10 @@ class CheckoutPage:
         self.wait = WebDriverWait(driver, 10)
 
     def fill_form(self, first_name: str, last_name: str, postal_code: str):
-        self.wait.until(EC.presence_of_element_located((By.ID, "first-name"))).send_keys(first_name)
-        self.driver.find_element(By.ID, "last-name").send_keys(last_name)
-        self.driver.find_element(By.ID, "postal-code").send_keys(postal_code)
-        self.driver.find_element(By.ID, "continue").click()
+        self.wait.until(EC.element_to_be_clickable((By.ID, "first-name"))).send_keys(first_name)
+        self.wait.until(EC.element_to_be_clickable((By.ID, "last-name"))).send_keys(last_name)
+        self.wait.until(EC.element_to_be_clickable((By.ID, "postal-code"))).send_keys(postal_code)
+        self.wait.until(EC.element_to_be_clickable((By.ID, "continue"))).click()
         return self
 
     def get_total(self) -> float:
